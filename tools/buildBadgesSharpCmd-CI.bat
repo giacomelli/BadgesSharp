2a55b07208240230158004a9f9e331029b761b87
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-15 09:29:36 -0300

    Fixing path.

--- tools/buildBadgesSharpCmd-CI.bat
+++ tools/buildBadgesSharpCmd-CI.bat
@@ -4,6 +4,6 @@ mkdir C:\projects\badgessharp\build\
 
 cd .\src\BadgesSharpCmd\bin\Release
 
-C:\projects\badgessharp\tools\ILRepack.exe /target:exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:.C:\projects\badgessharp\build\BadgesSharpCmd.exe /wildcards BadgesSharpCmd.exe *.dll
+C:\projects\badgessharp\tools\ILRepack.exe /target:exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:C:\projects\badgessharp\build\BadgesSharpCmd.exe /wildcards BadgesSharpCmd.exe *.dll
 
 cd C:\projects\badgessharp\
