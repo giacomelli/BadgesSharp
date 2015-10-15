c178afc1d3c804811a29537fcde725e29c8ec49b
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-15 13:32:01 -0300

    Fix folder.

--- tools/buildBadgesSharpCmd-CI.bat
+++ tools/buildBadgesSharpCmd-CI.bat
@@ -3,6 +3,6 @@ echo Building BadgesSharpCmd...
 mkdir C:\projects\badgessharp\build
 cd C:\projects\badgessharp\src\BadgesSharpCmd\bin\Release
 C:\projects\badgessharp\tools\ILRepack.exe /target:exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:C:\projects\badgessharp\build\BadgesSharpCmd.exe /wildcards BadgesSharpCmd.exe *.dll
-cd C:\projects\badgessharp\tools
+cd C:\projects\badgessharp
 
 echo done!
